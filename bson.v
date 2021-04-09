0b8d617cc680c93a6c57f7bbe656e5089eaf59a3
Carlos Esquerdo Bernat <esquerbatua@gmail.com>
2021-04-10 01:59:55 +0200

    More sugar fn

--- bson.v
+++ bson.v
@@ -41,13 +41,13 @@ pub fn (document &C.bson_t) as_canonical_extended_json() string {
 
 pub fn (document &C.bson_t) str() string {
 	return unsafe {
-		C.bson_as_json(document, 0).vstring()
+		C.bson_as_canonical_extended_json(document, 0).vstring()
 	}
 }
 
 pub fn (document &C.bson_t) as_json() string {
 	return unsafe {
-		C.bson_as_json(document, 0).vstring()
+		C.bson_as_canonical_extended_json(document, 0).vstring()
 	}
 }
 
--- cursor.v
+++ cursor.v
@@ -1,5 +1,20 @@
 module mongo
 
-pub fn (cursor &C.mongoc_cursor_t) next(document &&C.bson_t) bool {
+import json
+
+[inline]
+pub fn (cursor &C.mongoc_cursor_t) next_doc(document &&C.bson_t) bool {
 	return C.mongoc_cursor_next(cursor, document)
+}
+
+// Get the next document parsing it with the arg structure type and setting it
+pub fn (cursor &C.mongoc_cursor_t) next<T>(mut t T) ?bool {
+	document := C.bson_new()
+	if !cursor.next_doc(&document) {
+		return false
+	}
+	// declare str to avoid v errors
+	str := document.str()
+	t = json.decode(T, str) ?
+	return true
 }
