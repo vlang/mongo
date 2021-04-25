bf816bd7ab4df91c6071c53be7c0c93a35bc12b7
Carlos Esquerdo Bernat <esquerbatua@gmail.com>
2021-04-25 18:12:17 +0200

    Add local C headers

--- c_fn.v
+++ c_fn.v
@@ -1,9 +1,9 @@
 module mongo
 
-#flag -I/usr/local/include/libbson-1.0
-#flag -I/usr/local/include/libmongoc-1.0
-#flag -lmongoc-1.0
-#flag -lbson-1.0
+#flag -I @VMODROOT/thirdparty/libbson-1.0
+#flag -I @VMODROOT/thirdparty/libmongoc-1.0
+#flag -l mongoc-1.0
+#flag -l bson-1.0
 
 #include "mongoc/mongoc.h"
 #include "bson/bson.h"
