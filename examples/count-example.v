9a8fd2a6f36dac19383c9c86ed7f3dfa14c2b42a
enghitalo <enghitalo@gmail.com>
2022-09-28 10:00:56 -0300

    refactor: change 'new_from' function to 'new_bson_from'

--- examples/count-example.v
+++ examples/count-example.v
@@ -6,7 +6,9 @@ fn main() {
 	client := mongo.new_client(url)
 	collection := client.get_collection('teste', 'agreements')
 
-	bson_filter := mongo.new_from<map[string]string>({'version': '0.2.2'})
+	bson_filter := mongo.new_bson_from<map[string]string>({
+		'version': '0.2.2'
+	})
 
 	count := collection.count(bson_filter)
 
