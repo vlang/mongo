28b7c0f9c82e7a94eb0b2b15a2a56e8e83f4c4aa
enghitalo <enghitalo@gmail.com>
2022-10-01 12:27:22 -0300

    merge tests

--- src/collection_test.v
+++ src/collection_test.v
@@ -9,46 +9,12 @@ struct Test {
 	boolean bool
 }
 
-fn test_bson_t_insert() {
+fn test_collection() {
 	url := 'mongodb://127.0.0.1:27017/'
 
 	client := new_client(url)
 
-	collection := client.get_collection('vlang', 'mongo-test')
-
-	test := Test{
-		str: 'string'
-		number: int(2)
-		float: f64(2.1)
-		boolean: true
-	}
-
-	json_bson := new_from_json('{"str":"string","number":2,"float":2.1,"boolean":true}')
-
-	child := json2.Any({
-		'bar': json2.Any(10)
-	})
-
-	assert collection.insert_one({
-		'str':     'string'
-		'number':  2
-		'float':   2.1
-		'boolean': true
-		'foo':     child
-	})
-	assert collection.insert_one_from<Test>(test)
-	assert collection.insert_one_from_bson_t(json_bson)
-
-	json_bson.destroy()
 	client.get_database('vlang').drop()
-	client.destroy()
-}
-
-fn test_bson_t_find_lean() {
-	url := 'mongodb://127.0.0.1:27017/'
-
-	client := new_client(url)
-
 	collection := client.get_collection('vlang', 'mongo-test')
 
 	test := Test{
@@ -65,12 +31,39 @@ fn test_bson_t_find_lean() {
 		boolean: true
 	}
 
+	other_test := Test{
+		str: 'random'
+		number: 77
+		float: 985.36
+		boolean: false
+	}
+
 	bson_json_filter := new_from_json('{"str":"string"}')
+
+	json_bson := new_from_json('{"str":"string","number":2,"float":2.1,"boolean":true}')
+
+	child := json2.Any({
+		'bar': json2.Any(10)
+	})
+
+	assert collection.insert_one({
+		'number':  2
+		'float':   2.1
+		'boolean': true
+		'foo':     child
+	})
+	assert collection.insert_one_from<Test>(test)
+	assert collection.insert_one_from_bson_t(json_bson)
 
 	for i in 0 .. 3 {
 		print(i)
 		collection.insert_one_from<Test>(test)
+		collection.insert_one_from<Test>(other_test)
 	}
+	assert collection.count({}) == 9
+	assert collection.count({
+		'str': 'string'
+	}) == 5
 
 	lean_response_find := collection.find({
 		'str': 'string'
@@ -85,9 +78,9 @@ fn test_bson_t_find_lean() {
 	// // TODO - when x.json2.Any.str() can convert symbols like $
 	// assert response.str() == '[{"_id":{"\$oid":"633587f719a6f14926055715"},"str":"string","number":2,"float":2.1,"boolean":true},{"_id":{"$oid":"633587f719a6f14926055716"},"str":"string","number":2,"float":2.1,"boolean":true},{"_id":{"\$oid":"633587f719a6f14926055717"},"str":"string","number":2,"float":2.1,"boolean":true}]'
 
-	assert lean_response_find.len == 3
-	assert lean_response_find_from.len == 3
-	assert lean_response_find_from_bson_t.len == 3
+	assert lean_response_find.len == 5
+	assert lean_response_find_from.len == 5
+	assert lean_response_find_from_bson_t.len == 5
 
 	for obj in lean_response_find {
 		assert obj.as_map()['str'] or { 0 }.str() == 'string'
