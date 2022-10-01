e8168179a6298eaff404734c9ccc58217cc5b2da
enghitalo <enghitalo@gmail.com>
2022-10-01 07:58:30 -0300

    update mongo version from 1.16.2 to 1.21.0

--- src/thirdparty/libbson-1.0/bson/bson-config.h
+++ src/thirdparty/libbson-1.0/bson/bson-config.h
@@ -43,26 +43,6 @@
 
 
 /*
- * Define to 1 if we have access to GCC 32-bit atomic builtins.
- * While this requires GCC 4.1+ in most cases, it is also architecture
- * dependent. For example, some PPC or ARM systems may not have it even
- * if it is a recent GCC version.
- */
-#define BSON_HAVE_ATOMIC_32_ADD_AND_FETCH 1
-#if BSON_HAVE_ATOMIC_32_ADD_AND_FETCH != 1
-# undef BSON_HAVE_ATOMIC_32_ADD_AND_FETCH
-#endif
-
-/*
- * Similarly, define to 1 if we have access to GCC 64-bit atomic builtins.
- */
-#define BSON_HAVE_ATOMIC_64_ADD_AND_FETCH 1
-#if BSON_HAVE_ATOMIC_64_ADD_AND_FETCH != 1
-# undef BSON_HAVE_ATOMIC_64_ADD_AND_FETCH
-#endif
-
-
-/*
  * Define to 1 if you have clock_gettime() available.
  */
 #define BSON_HAVE_CLOCK_GETTIME 1
@@ -108,15 +88,6 @@
 
 
 /*
- * Define to 1 if you have reallocf available on your platform.
- */
-#define BSON_HAVE_REALLOCF 0
-#if BSON_HAVE_REALLOCF != 1
-# undef BSON_HAVE_REALLOCF
-#endif
-
-
-/*
  * Define to 1 if you have struct timespec available on your platform.
  */
 #define BSON_HAVE_TIMESPEC 1
@@ -135,18 +106,17 @@
 
 
 /*
- * Define to 1 if you have SYS_gettid syscall
+ * Define to 1 if you have rand_r available on your platform.
  */
-#define BSON_HAVE_SYSCALL_TID 1
-#if BSON_HAVE_SYSCALL_TID != 1
-# undef BSON_HAVE_SYSCALL_TID
-#endif
-
 #define BSON_HAVE_RAND_R 1
 #if BSON_HAVE_RAND_R != 1
 # undef BSON_HAVE_RAND_R
 #endif
 
+
+/*
+ * Define to 1 if you have strlcpy available on your platform.
+ */
 #define BSON_HAVE_STRLCPY 0
 #if BSON_HAVE_STRLCPY != 1
 # undef BSON_HAVE_STRLCPY
